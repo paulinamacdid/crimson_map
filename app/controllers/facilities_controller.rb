f835e0755553bc657bfe548a6d7c2d19f6360366
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-16 15:13:10 +0100

    fix bugs in map and added custom pin

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -5,7 +5,8 @@ class FacilitiesController < ApplicationController
       {
         lat: facility.latitude,
         lng: facility.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {facility: facility})
+        info_window_html: render_to_string(partial: "info_window", locals: {facility: facility}),
+        marker_html: render_to_string(partial: "marker", locals: {facility: facility})
       }
     end
   end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,9 +2,9 @@ class Facility < ApplicationRecord
   has_many :favourites
   has_many :reviews
   has_many :routes
-  validates :address, :business_owner, :sanitary_products, :baby_change, :quiet_place, :education, presence: true
+  validates :address, :sanitary_products, :baby_change, :quiet_place, presence: true
+  validates :toilet, presence: true, numericality: { only_integer: true }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  validates :toilet, presence: true, numericality: { only_integer: true }
 end
