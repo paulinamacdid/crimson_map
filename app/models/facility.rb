fc3789cb4d87d0c7daf7c57134a5c2faabd6bd81
Paulina Macdid <paulinamacdid@hotmail.co.uk>
2024-10-16 15:09:23 +0100

    seed errors update

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,7 +2,7 @@ class Facility < ApplicationRecord
   has_many :favourites
   has_many :reviews
   has_many :routes
-  validates :address, :business_owner, :sanitary_products, :baby_change, :quiet_place, :education, presence: true
+  validates :address, :sanitary_products, :baby_change, :quiet_place, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
