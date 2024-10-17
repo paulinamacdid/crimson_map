445e08672acc1ed4f4e704d854ff8f048059df86
Paulina Macdid <paulinamacdid@hotmail.co.uk>
2024-10-17 18:06:46 +0100

    reviews backend with show page functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,14 @@ class PagesController < ApplicationController
 
   def home
     @facilities = Facility.all
+    @markers = @facilities.geocoded.map do |facility|
+      {
+        lat: facility.latitude,
+        lng: facility.longitude,
+        info_window_html: render_to_string(partial: "facilities/info_window", locals: { facility: facility }),
+        marker_html: render_to_string(partial: "facilities/marker", locals: { facility:  facility })
+      }
+    end
     Facility.near('London, NE, UK')
     # add line of code that only pulls facilities that are a rating 4 and above
   end
