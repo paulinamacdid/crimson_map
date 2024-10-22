110333f505e596b8e45ded0eee5e92874f1529c5
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-22 12:46:57 +0100

    add stars and half-stars to ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :facility
   validates :comment, :rating, presence: true
-  validates :rating, numericality: { in: 0..5 }
+  validates :rating, numericality: { in: 1..5 }
   validates :rating, numericality: { only_integer: true }
   validates :comment, length: { minimum: 10 }
 end
