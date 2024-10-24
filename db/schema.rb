2dc06a83c537fa4c939e2731f75c320d3e2615de
Katherine Heard <katherinebeheard@gmail.com>
2024-10-24 09:15:54 +0100

    started blog styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_22_144254) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_074143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_22_144254) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "content_preview"
   end
 
   create_table "facilities", force: :cascade do |t|
